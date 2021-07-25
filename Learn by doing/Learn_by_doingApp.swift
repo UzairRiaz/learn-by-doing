310c5dbb4d6e6cd11be415c149919c64edfd7296
uzairriaz <uzairriaz.pk@gmail.com>
2021-07-25 13:04:03 +0500

    Initial Commit

---
+++ Learn by doing/Learn_by_doingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learn_by_doingApp.swift
+//  Learn by doing
+//
+//  Created by Umair Riaz on 25/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct Learn_by_doingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
